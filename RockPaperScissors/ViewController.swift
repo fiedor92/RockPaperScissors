18e402a07bd783c2c56fdaea0e6d2e50239c9343
Maciej <appacmp@appacmps-MacBook-Pro.local>
2015-07-26 12:08:01 +0200

    Wyswietlanie na telefonie

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var highScoreLabel: UILabel!
+    let defaults = NSUserDefaults(suiteName: "group.com.gmail.macfiedo.RockPaperScissors")!
+    
+    func updateHighScore(){
+        if let highscore = defaults.objectForKey("highscore") as? Int{
+            highScoreLabel.text = "Watch HighScore: \(highscore)"
+        }else{
+            highScoreLabel.text = "No HighScore yet!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateHighScore()
     }
 
 
