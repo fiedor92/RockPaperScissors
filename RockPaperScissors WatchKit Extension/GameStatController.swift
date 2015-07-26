9811be0bddcfce68fe085a5f5a7d07b23357aa0f
Maciej <appacmp@appacmps-MacBook-Pro.local>
2015-07-26 11:20:41 +0200

    Dzialajaca gra

---
+++ RockPaperScissors WatchKit Extension/GameStatController.swift
@@ -0,0 +1,30 @@
+//
+//  GameStatContorller.swift
+//  RockPaperScissors WatchKit Extension
+//
+//  Created by appacmp on 26/07/15.
+//  Copyright (c) 2015 Maciej Fiedorowicz. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class GameStatController: WKInterfaceController{
+    
+    @IBOutlet weak var gamesPlayedLabel: WKInterfaceLabel!
+    @IBOutlet weak var highScoreLabel: WKInterfaceLabel!
+    @IBOutlet weak var currentScoreLabel: WKInterfaceLabel!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        if let newDict = context as? [String: String],
+            currentScore = newDict["currentScore"],
+            highScore = newDict["highScore"],
+            gamePlayed = newDict["gamePlayes"]
+        {
+            gamesPlayedLabel.setText(gamePlayed)
+            highScoreLabel.setText(highScore)
+            currentScoreLabel.setText(currentScore)
+        }
+        
+    }
+}
--- RockPaperScissors WatchKit Extension/InterfaceController.swift
+++ RockPaperScissors WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,89 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
+    @IBOutlet weak var oponentSymbol: WKInterfaceImage!
+    @IBOutlet weak var resultLabel: WKInterfaceLabel!
+    @IBOutlet weak var scoreLabel: WKInterfaceLabel!
+    
+    let defaults = NSUserDefaults(suiteName: "group.com.gmail.macfiedo.RockPaperScissors")!
+    
+    var score = 0
+    var gamesCounter = 0
+    var highScore = 0
+    let symbols = ["rock", "paper", "scissors"]
+    
+    @IBAction func choosePaper() {
+        play("paper")
+    }
+    @IBAction func chooseRock() {
+        play("rock")
+    }
+    
+    @IBAction func chooseScissors() {
+        play("scissors")
+    }
+    
+    @IBAction func showGameStats() {
+        highScore  = getHighScoreFromDefaults()
         
-        // Configure interface objects here.
+        pushControllerWithName("GameStat", context: ["currentScore":"\(score)", "highScore:": "\(highScore)", "gamePlayes":"\(gamesCounter)"])
     }
+    func play(symbol: String) {
+        gamesCounter = gamesCounter + 1
+        
+        let oponentPick = symbols[Int(arc4random_uniform(3))]
+        oponentSymbol.setImage(UIImage(named: oponentPick))
+        
+        if oponentPick == symbol {
+            resultLabel.setText("Tie. +1")
+            score += 1
+        }
+        else{
+            var won = false
+            switch symbol{
+                case "rock":
+                    if oponentPick == "scissors" {
+                        won = true
+                }
+                case "paper":
+                    if oponentPick == "rock"{
+                        won = true
+                }
+                case "scissors":
+                    if oponentPick == "paper"{
+                        won = true
+                }
+            default: resultLabel.setText("Unrecognized symbol")
+            }
+            if won {
+                resultLabel.setText("You won! +5")
+                score += 5
+            }
+            else{
+                resultLabel.setText("You lost! -2")
+                score -= 2
+            }
+            
+        }
+        scoreLabel.setText("\(score)")
+        updateHighScore()
 
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    
+    func getHighScoreFromDefaults() -> Int {
+        var highScoreFromDefaults = defaults.objectForKey("highscore") as? Int
+        
+        return (highScoreFromDefaults != nil) ? highScoreFromDefaults! : 0
+    }
+    
+    func updateHighScore(){
+        highScore = getHighScoreFromDefaults()
+        
+        if (score > highScore){
+            defaults.setObject(score, forKey: "highscore")
+            defaults.synchronize()
+        }
+        
     }
 
 }
